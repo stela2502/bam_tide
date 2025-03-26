00a79304a56503e911f8cd0d029694a3d31de47e
Stefan Lang <Stefan.Lang@med.lu.se>
2025-03-26 11:08:23 +0100

    disengaged 10xbamtofastq.

--- src/bin/10xbam2fastq.rs
+++ src/bin/10xbam2fastq.rs
@@ -9,7 +9,7 @@ use bam_tide::read_data::ReadData;
 
 use std::time::SystemTime;
 
-
+/// Used to regenerate the R1 and R2 fastq files from a CellRanger bam file.
 use clap::{Parser};
 #[derive(Parser)]
 #[clap(version = "0.4.3", author = "Stefan L. <stefan.lang@med.lu.se>")]
@@ -28,6 +28,8 @@ struct Opts {
 fn main(){
 	let now = SystemTime::now();
     let opts: Opts = Opts::parse();
+
+    panic!("Do not use this program! use https://github.com/10XGenomics/bamtofastq\nIf you are on COSMOS-SENS bamtofastq is installed in the Rustody/1.4 module.");
 
     if ! Path::new(&opts.bam).exists() {
         panic!("I need a bam file to work on!")
@@ -51,20 +53,23 @@ fn main(){
     };
 
     // Gzipped FASTQ output files
-    let f1_str = format!("{}_{}", &opts.prefix, "R1.fastq.gz");
+    // CellRanger whnts this file name structure: SampleName_S1_L001_R[12]_001.fastq.gz
+    let f1_str = format!("{}_{}", &opts.prefix, "S1_L001_R1_001.fastq.gz");
     let f1 = File::create(&f1_str).expect(&format!("Failed to create ofile {} output file", &f1_str ));
     let buff_f1 = BufWriter::new( f1 );
     let mut fq1_writer = GzEncoder::new( buff_f1, Compression::default() );
     
-    let f2_str = format!("{}_{}", &opts.prefix, "R2.fastq.gz");
+    let f2_str = format!("{}_{}", &opts.prefix, "S1_L001_R2_001.fastq.gz");
     let f2 = File::create(&f2_str).expect(&format!("Failed to create ofile {} output file", &f2_str ));
     let buff_f2 = BufWriter::new(f2);
     let mut fq2_writer = GzEncoder::new( buff_f2, Compression::default() );
 
-    let i1_str = format!("{}_{}", &opts.prefix, "I1.fastq.gz");
+    /*
+    let i1_str = format!("{}_{}", &opts.prefix, "S1_L001_I1_001.fastq.gz");
     let i1 = File::create(&i1_str).expect(&format!("Failed to create ofile {} output file", &i1_str ));
     let buff_i1 = BufWriter::new(i1);
     let mut fi1_writer = GzEncoder::new( buff_i1, Compression::default() );
+    */
 
     for r in reader.records() {
     	let mut record = r.expect("Bam read could not be collected");
@@ -86,6 +91,10 @@ fn main(){
             ) 
         ).expect("Faled to write data to read2");
 
+        /*
+        // The I1 fastq file does only contain the sample sequencing tag and is not necessary any more.
+        // The data for that is also not sotored in every bam file any more.
+        // I can not regenerate that here.
         writeln!(fi1_writer, "{}", as_fastq(
             &qname, 
             get_tag_value( &record, b"UR" ).unwrap_or_else(|| panic!("UR tag not found?! {:?}", &qname )  ).as_bytes(),
@@ -96,6 +105,7 @@ fn main(){
             */
             ) 
         ).expect("Faled to write data to read2");
+        */
     }
 
     println!("R1/R2/I1 triplet FASTQ files written and gzipped.");
